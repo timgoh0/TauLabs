9ea18306dc9c7f49f18a0bd3d2f4e29ba61dc7e8
James Cotton <peabody124@gmail.com>
2013-02-20 20:51:37 -0600

    QT5: Rename plugins

--- ground/gcs/src/plugins/gcscontrol/gcscontrolplugin.h
+++ ground/gcs/src/plugins/gcscontrol/gcscontrolplugin.h
@@ -39,7 +39,7 @@ class GCSControlGadgetFactory;
 class GCSControlPlugin : public ExtensionSystem::IPlugin
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "AboveGroundLabs.plugins.GCSControlPlugin" FILE "GCSControl.json")
+    Q_PLUGIN_METADATA(IID "TauLabs.plugins.GCSControlPlugin" FILE "GCSControl.json")
 
 public:
     GCSControlPlugin();
